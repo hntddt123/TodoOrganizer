e28cf58f16989d61d9446ce4e7376c0167a87d6f
Nientai Ho <hntddt123@gmail.com>
2022-04-23 16:44:26 -0500

    Added dashboard routes, navigation and history

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Dashboard from './Dashboard';
+import Navigation from './Navigation';
 
-import { store } from '../../redux/store/index';
+import { store } from '../redux/store/index';
+import { history } from '../redux/store/history';
 
 function App() {
   return (
-    <Provider store={store}>
-      <Dashboard />
-    </Provider>
+    <BrowserRouter history={history}>
+      <Provider store={store}>
+        <Navigation />
+        <Routes>
+          <Route exact path='/dashboard' element={<Dashboard />} />
+        </Routes>
+      </Provider>
+    </BrowserRouter>
   );
 }
 export default App;
---
+++ src/app/components/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+function Navigation() {
+
+  return (
+    <div>
+      <Link to='/dashboard'>
+        <h1>
+          TodoList
+        </h1>
+      </Link>
+    </div>
+  );
+}
+
+export default Navigation;
