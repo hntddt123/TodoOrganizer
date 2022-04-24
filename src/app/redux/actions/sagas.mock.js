1bd96b37a9490e3db8047903c8c59480728a9cf5
Nientai Ho <hntddt123@gmail.com>
2022-04-24 13:01:08 -0500

    Updated sagas to take action.payload from redux

--- src/app/redux/actions/sagas.mock.js
+++ src/app/redux/actions/sagas.mock.js
@@ -6,12 +6,12 @@ import * as taskActions from './taskActions';
 
 export function* taskCreationSaga() {
   while (true) {
-    const { groupID } = yield take(taskActionType.REQUEST_NEW_TASK);
+    const { payload } = yield take(taskActionType.REQUEST_NEW_TASK);
     const taskID = uuid();
     const ownerID = `User1`;
 
-    yield put(taskActions.createNewTask(taskID, groupID, ownerID));
+    yield put(taskActions.createNewTask(taskID, payload.groupID, ownerID));
 
-    console.log(`GroupID from taskCreationSaga is ${groupID}`);
+    console.log(`GroupID from taskCreationSaga is ${payload.groupID}`);
   }
 }
